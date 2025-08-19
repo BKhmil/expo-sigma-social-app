9472790c8b43c26168c3739515a96620429c9a4b
Bohdan Khmil <khmil.work@gmail.com>
2025-08-19 19:23:25 +0300

    (feat): add expected tables definitions | add web-hook for checking Clerk's [user.created] event | add [logout] button for tests

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native';
+import { useAuth } from '@clerk/clerk-expo';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 const HomeScreen = () => {
+	const { signOut } = useAuth();
 	return (
 		<View>
-			<Text>Home</Text>
+			<TouchableOpacity onPress={() => signOut()}>
+				<Text>logout</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
