6409c7837c031c96b1eec09c92ee5d9ea08c487c
Bohdan Khmil <khmil.work@gmail.com>
2025-08-21 19:06:26 +0300

    (feat): implement FeedScreen | add icons | add splash screen | add HetBrainsMono-Medium font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,33 @@
 import InitialLayout from '@/components/InitialLayout';
 import ClerkAndConvexProvider from '@/providers/ClerkAndConvexProvider';
+import { useFonts } from 'expo-font';
+import { SplashScreen } from 'expo-router';
+import { useCallback } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function RootLayout() {
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+	const [fontsLoaded] = useFonts({
+		'JetBrainsMono-Medium': require('../assets/fonts/JetBrainsMono-Medium.ttf'),
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (true) await SplashScreen.hideAsync();
+	}, [fontsLoaded]);
+
 	return (
 		<ClerkAndConvexProvider>
 			<SafeAreaProvider>
-				<SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+				<SafeAreaView
+					style={{ flex: 1, backgroundColor: '#000' }}
+					onLayout={onLayoutRootView}
+				>
 					<InitialLayout />
 				</SafeAreaView>
 			</SafeAreaProvider>
 		</ClerkAndConvexProvider>
 	);
-}
+};
+
+export default RootLayout;
